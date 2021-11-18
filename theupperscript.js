66d6ed476bc1a5bf031478a80e82d41e4c54210d
JS15 <76090918+CeoOfFry@users.noreply.github.com>
2021-11-18 14:45:53 +0800

    pag
    a

--- theupperscript.js
+++ theupperscript.js
@@ -1,40 +1,51 @@
 console.clear();
 
-let link = window.location.href
+let L = window.location.href
     .replace("/en/exams", "/qlearn/v1/schedule")
     .split("/questions")[0] + "/usage";
 
-let headers = new Headers({
+let H = new Headers({
     "Authorization": "Token xd31sYEPWvR+ZWzJlLoMMqTr2XooH2uRZ5zKHDSkRyppdEB4116XvMRUp+RC3/6h1hbRzD/la4+pK6/4WeCVxw=="
 });
 
-let response = await fetch(link,
+let R = await fetch(L,
     {
-        headers: headers,
+        headers: H,
         method: "GET"
     });
 
-let u = await response.json();
+let O = await R.json();
+let o = O[0];
 
-console.log("[" + link + "]\n\n");
+console.log("[" + L + "]\n\n");
+console.log("-\n\n[" + o.name + "]");
 
-let wrong = "incorrectly answered questions: \n";
-let unanswered = "unanswered questions: \n";
-for (let i = 0; i < Object.keys(u[0].last_attempt_scores).length; i++) {
-    console.log(i + 1, u[0].last_attempt_scores[i]);
-    if (u[0].last_attempt_scores[i] == "incorrect") {
-        wrong = wrong + (i + 1).toString() + "\n";
+let wr = "[❌ incorrectly answered questions]: \n";
+let un = "[unanswered questions]: \n";
+for (let i = 0; i < Object.keys(o.last_attempt_scores).length; i++) {
+    let _a = o.last_attempt_scores[i];
+
+    console.log(i + 1,
+        (_a == "previously_correct" ? "✔️ correct" :
+            _a == "incorrect" ? "❌ wrong" :
+                _a == null ? "[unanswered]" :
+                    null)
+    );
+
+    if (_a == "incorrect") {
+        wr = wr + (i + 1).toString() + "\n";
     }
-    else if (u[0].last_attempt_scores[i] == null) {
-        unanswered = unanswered + (i + 1).toString() + "\n";
+    else if (_a == null) {
+        un = un + (i + 1).toString() + "\n";
     }
 }
 
 
 console.log(
-    "\n| current %", u[0].correct_percent,
-    "\n| answered questions", u[0].questions_answered_count, "/", u[0].questions_count,
-    "\n| correctly answered questions", u[0].questions_answered_count * (u[0].correct_percent / 100), "/", u[0].questions_count);
+    "\n| current %", o.correct_percent,
+    "\n| answered questions", o.questions_answered_count, "/", o.questions_count,
+    "\n| correctly answered questions", o.questions_answered_count * (o.correct_percent / 100), "/", o.questions_count
+);
 
-if (wrong != "incorrectly answered questions: \n") console.log("\n" + wrong);
-if (unanswered != "unanswered questions: \n") console.log("\n" + unanswered);
+if (wr != "[❌ incorrectly answered questions]: \n") console.log("\n" + wr);
+if (un != "[unanswered questions]: \n") console.log("\n" + un);
